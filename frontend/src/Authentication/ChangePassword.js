440ce17ce4c250a7475c3e8b73543c2e350364b7
muhammadasif-wd <muhammadasif.wd@gmail.com>
2023-04-16 05:25:16 +0600

    done fully complete project

--- frontend/src/Authentication/ChangePassword.js
+++ frontend/src/Authentication/ChangePassword.js
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Loader from "../Shared/Loader";
+import { ServerAPI } from "../API/ServerAPI";
+import AuthUser from "../Hooks/AuthUser";
+import Swal from "sweetalert2";
 
 const ChangePassword = () => {
+  const { userInfo } = AuthUser()
   //loading implement
   const [loading, setLoading] = useState(true);
   useEffect(() => {
@@ -9,6 +13,59 @@ const ChangePassword = () => {
       setLoading(false);
     }, 500);
   });
+
+  const handleChangePassword = (e) => {
+    e.preventDefault()
+    const currentPassword = e?.target?.currentPassword?.value
+    const newPassword = e?.target?.newPassword?.value
+    const confirmPassword = e?.target?.confirmPassword?.value
+    const data = { currentPassword, newPassword, confirmPassword }
+    if (newPassword === confirmPassword) {
+      // Post identity Data in database
+      fetch(`${ServerAPI}/user/change-password/${userInfo?._id}`, {
+        method: "PATCH",
+        headers: {
+          'content-type': 'application/json',
+          authorization: `Bearer ${localStorage.getItem('access')}`
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(res => {
+          if (res?.status === "success") {
+            Swal.fire({
+              title: `${res?.status}`,
+              text: `${res?.message}`,
+              icon: "success",
+              confirmButtonText: "Thank you!",
+              confirmButtonColor: "#374151",
+              background: "#1f2937",
+              color: "#fff",
+            });
+          } else {
+            Swal.fire({
+              title: `${res?.status}`,
+              text: `${res?.message}`,
+              icon: "error",
+              confirmButtonText: "Try again!",
+              confirmButtonColor: "#374151",
+              background: "#1f2937",
+              color: "#fff",
+            });
+          }
+        })
+    } else {
+      Swal.fire({
+        title: `Error!`,
+        text: `Your new password and confirm password doe's not matched!`,
+        icon: "error",
+        confirmButtonText: "Try again!",
+        confirmButtonColor: "#374151",
+        background: "#1f2937",
+        color: "#fff",
+      });
+    }
+  }
   return (
     <>
       {loading ? (
@@ -16,12 +73,12 @@ const ChangePassword = () => {
       ) : (
         <div>
           <h1>Change Password</h1>
-          <form action="">
+          <form action="" onSubmit={handleChangePassword}>
             <div className="flex flex-col gap-2 border border-dark/50 rounded-md md:w-1/2 mt-20 p-5">
               <p className="text-2xl text-center py-3">Change Password</p>
               <input
                 type="password"
-                name="password"
+                name="currentPassword"
                 id="password"
                 className="border border-dark/50 rounded-md p-1 outline-none"
                 placeholder="type your current password"
@@ -43,7 +100,7 @@ const ChangePassword = () => {
               <input
                 type="submit"
                 value="Confirm"
-                className="bg-info py-2 text-white rounded-md"
+                className="bg-info py-2 text-white rounded-md cursor-pointer"
               />
             </div>
           </form>
